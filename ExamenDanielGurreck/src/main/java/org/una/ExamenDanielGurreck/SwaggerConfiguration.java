6f7ddf8c511bd7ab75f7ffe440892b8faad05b9f
Gurreck <daniel.gurreck.gonzalez@est.una.ac.cr>
2020-10-27 11:15:20 -0600

    Se realizo la configuración basica

---
+++ ExamenDanielGurreck/src/main/java/org/una/ExamenDanielGurreck/SwaggerConfiguration.java
@@ -0,0 +1,46 @@
+package org.una.ExamenDanielGurreck;
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(
+                        RequestHandlerSelectors
+                                .basePackage("org.una.ExamenDanielGurreck.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .tags(new Tag("Categorias Web", "Entidad de Categorias Web"),
+                        new Tag("Visitas Web", "Entidad de Visitas Web"),
+                        new Tag("Paginas Web", "Entidad de Paginas Web"),
+                        new Tag("Navegadores", "Entidad de Navegadores"),
+                        new Tag("Paginas Web Categorias Web", "Entidad de Navegadores")
+                );
+
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Examen Final",
+                "Rest API del requerimiento 7.3.",
+                "Versión:2.1.0",
+                "https://google.com",
+                new Contact("UNA Sede Región Brunca", "https://srb.una.ac.cr/index.php/es/", "decanatosrb@una.cr"),
+                "Apache-2.0", "http://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList());
+    }
+}
