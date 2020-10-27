d343977dc5b8fbf48f9c7a0881228e60d754b096
Gurreck <daniel.gurreck.gonzalez@est.una.ac.cr>
2020-10-26 20:42:48 -0600

    Se creo el proyecto

---
+++ ExamenDanielGurreck/src/test/java/org/una/ExamenDanielGurreck/ExamenDanielGurreckApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.ExamenDanielGurreck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenDanielGurreckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
